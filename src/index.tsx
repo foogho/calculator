ca68b8dce67139dcd2e790d266b895d2bae6cb7f
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-01-09 22:48:59 +0330

    Update index.html,index.tsx
    - place FCC tests script into index.html
    
    - use legacy ReactDOM.render as a hack for resolving FCC test failure
    
    - for more information refer to index.tsx comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+// below render is the default code when creating projects
+// via create-react-app but when using it the FCC tests fails
+// this post recommends using legacy render method:
+// https://forum.freecodecamp.org/t/build-a-javascript-calculator-tests-wont-pass/559466
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
